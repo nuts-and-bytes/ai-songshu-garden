b6e288d57f031a4da847b88f35ca4d1e6e84a7ec
Xinyao <zhuxinyao99@gmail.com>
2026-06-04 17:20:32 +0800

    add pageTitleSuffix

--- quartz.config.ts
+++ quartz.config.ts
@@ -4,7 +4,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "AI 与一只松鼠",
-    pageTitleSuffix: "",
+    pageTitleSuffix: " · AI 与一只松鼠",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
